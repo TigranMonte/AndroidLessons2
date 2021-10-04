6cda2fbd056adf7a24b16193c9938db4d73e1454
Tigran <tigranma@gmail.com>
2021-10-04 22:31:59 +0300

    Bits and Pizzas app

--- bitsandpizza/src/main/java/ru/startandroid/bitsandpizza/MainActivity.java
+++ bitsandpizza/src/main/java/ru/startandroid/bitsandpizza/MainActivity.java
@@ -3,7 +3,10 @@ package ru.startandroid.bitsandpizza;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
