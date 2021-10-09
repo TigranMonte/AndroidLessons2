0208cc1006ded95ffa927ce3bee3f6a498c827f6
Tigran <tigranma@gmail.com>
2021-10-09 18:43:36 +0300

    Lesson 104 Fragments. Lifecycle

---
+++ p1041fragmentlifecycle/src/main/java/ru/startandroid/p1041fragmentlifecycle/Fragment1.java
@@ -0,0 +1,77 @@
+package ru.startandroid.p1041fragmentlifecycle;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Fragment1 extends android.app.Fragment {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(LOG_TAG, "Fragment1 onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment1 onCreate");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Fragment1 onCreateView");
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment1 onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "Fragment1 onStart");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "Fragment1 onResume");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "Fragment1 onPause");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "Fragment1 onStop");
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(LOG_TAG, "Fragment1 onDestroyView");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "Fragment1 onDestroy");
+    }
+
+    public void onDetach() {
+        super.onDetach();
+        Log.d(LOG_TAG, "Fragment1 onDetach");
+    }
+}
+
