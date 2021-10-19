0c7b7d77095770d33e42a0b1563c9efe3ced4ce7
Tigran <tigranma@gmail.com>
2021-10-19 21:41:53 +0300

    Lesson 109 ListFragment - список

---
+++ p1091listfragment/src/main/java/ru/startandroid/p1091listfragment/MainList.java
@@ -0,0 +1,39 @@
+package ru.startandroid.p1091listfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class MainList extends ListFragment {
+
+    String data [] = new String[] {"one", "two", "three", "four"};
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_activated_1, data);
+        setListAdapter(adapter);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, null);
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Toast.makeText(getActivity(), "position = " + position, Toast.LENGTH_SHORT).show();
+    }
+}
