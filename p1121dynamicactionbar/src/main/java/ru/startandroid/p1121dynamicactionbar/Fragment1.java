81b3f2a2ae8d368e260d2352fa7cbd9063d1cd94
Tigran <tigranma@gmail.com>
2021-10-22 23:09:36 +0300

    Lesson 112 ActionBar. Динамическое размещение элементов

---
+++ p1121dynamicactionbar/src/main/java/ru/startandroid/p1121dynamicactionbar/Fragment1.java
@@ -0,0 +1,32 @@
+package ru.startandroid.p1121dynamicactionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment1, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
