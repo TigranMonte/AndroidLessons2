c957c9b62b16430881ce28b9f18126ae29ebf7b2
Tigran <tigranma@gmail.com>
2021-11-02 17:17:49 +0300

    Lesson 118 Виджеты. Конфигурационный экран

--- p1173simplewidget3/src/main/java/ru/startandroid/p1173simplewidget3/ExampleAppWidgetProvider.java
+++ p1173simplewidget3/src/main/java/ru/startandroid/p1173simplewidget3/ExampleAppWidgetProvider.java
@@ -1,10 +1,14 @@
 package ru.startandroid.p1173simplewidget3;
 
+import static ru.startandroid.p1173simplewidget3.ExampleAppWidgetConfig.KEY_BUTTON_TEXT;
+import static ru.startandroid.p1173simplewidget3.ExampleAppWidgetConfig.SHARED_PRES;
+
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
@@ -15,8 +19,12 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
+            SharedPreferences prefs = context.getSharedPreferences(SHARED_PRES, Context.MODE_PRIVATE);
+            String buttonText = prefs.getString(KEY_BUTTON_TEXT + appWidgetId, "Press me");
+
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
             views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+            views.setCharSequence(R.id.example_widget_button, "setText", buttonText);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
