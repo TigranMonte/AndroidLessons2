07c64aeb519f38353f7f607d72c443e9af16058c
Tigran <tigranma@gmail.com>
2021-11-03 21:15:06 +0300

    Lesson 119 Виджеты. Изменение размеров

--- p1173simplewidget3/src/main/java/ru/startandroid/p1173simplewidget3/ExampleAppWidgetProvider.java
+++ p1173simplewidget3/src/main/java/ru/startandroid/p1173simplewidget3/ExampleAppWidgetProvider.java
@@ -9,13 +9,17 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
+            Toast.makeText(context, "onUpdate", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
@@ -26,8 +30,46 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
             views.setCharSequence(R.id.example_widget_button, "setText", buttonText);
 
+            Bundle appWidgetOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
+            resizeWidget(appWidgetOptions, views);
+
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
         }
+    }
+
+    private void resizeWidget(Bundle appWidgetOptions, RemoteViews views) {
+        int minWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int maxWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+        int minHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int maxHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+
+        if (maxHeight > 100) {
+            views.setViewVisibility(R.id.example_widget_text, View.VISIBLE);
+        } else {
+            views.setViewVisibility(R.id.example_widget_text, View.GONE);
+        }
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+        resizeWidget(newOptions, views);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Toast.makeText(context, "onDeleted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Toast.makeText(context, "onEnabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        Toast.makeText(context, "onDisabled", Toast.LENGTH_SHORT).show();
     }
 }
