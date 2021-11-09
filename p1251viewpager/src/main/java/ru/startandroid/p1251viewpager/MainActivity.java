58357fe542d047f0ea09ceb340c910409fd14f5d
Tigran <tigranma@gmail.com>
2021-11-09 21:46:00 +0300

    Lesson 125 ViewPager (with PagerTitleStrip)

---
+++ p1251viewpager/src/main/java/ru/startandroid/p1251viewpager/MainActivity.java
@@ -0,0 +1,71 @@
+package ru.startandroid.p1251viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String TAG = "myLogs";
+    static final int PAGE_COUNT = 10;
+
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Title " + position;
+        }
+    }
+
+}
---
+++ p1251viewpager/src/main/java/ru/startandroid/p1251viewpager/PageFragment.java
@@ -0,0 +1,51 @@
+package ru.startandroid.p1251viewpager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Random;
+
+public class PageFragment extends Fragment {
+
+    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+
+    int pageNumber;
+    int backColor;
+
+    static PageFragment newInstance(int page) {
+        PageFragment pageFragment = new PageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+
+        Random rnd = new Random();
+        backColor = Color.argb(40,rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null);
+
+        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+        tvPage.setText("Page " + pageNumber);
+        tvPage.setBackgroundColor(backColor);
+
+        return view;
+    }
+}
