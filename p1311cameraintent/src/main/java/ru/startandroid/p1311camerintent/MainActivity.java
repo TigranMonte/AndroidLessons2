575ba5e1a2b622d85fd9760211ef65c4da72bc7f
Tigran <tigranma@gmail.com>
2021-12-06 15:59:42 +0300

    Lesson 131 Camera. Image capture

---
+++ p1311cameraintent/src/main/java/ru/startandroid/p1311camerintent/MainActivity.java
@@ -0,0 +1,63 @@
+package ru.startandroid.p1311camerintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+        btOpen = (Button) findViewById(R.id.bt_open);
+
+        // request for camera permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.CAMERA
+            }, 100);
+        }
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            // get capture image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            // set capture image to image view
+            imageView.setImageBitmap(captureImage);
+        }
+    }
+}
