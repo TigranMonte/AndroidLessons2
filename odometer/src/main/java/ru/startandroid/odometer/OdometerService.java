04e002b522b6be4e17e2d96ea73fbcb60dfc18af
Tigran <tigranma@gmail.com>
2021-12-16 22:42:58 +0300

    Odometer app (Binding service)

---
+++ odometer/src/main/java/ru/startandroid/odometer/OdometerService.java
@@ -0,0 +1,29 @@
+package ru.startandroid.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+public class OdometerService extends Service {
+
+    private final IBinder binder = (IBinder) new OdometerService();
+    private final Random random = new Random();
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public double getDistance() {
+        return random.nextDouble();
+    }
+}
