1c72a658fda30c19c810e9db2c5fc24ef6955bfa
Tigran <tigranma@gmail.com>
2022-01-10 16:41:33 +0300

    Lesson 141 Рисование, Доступ к Canvas (2-ый способ через SurfaceView)

--- p1411canvasview/src/main/java/ru/startandroid/p1411canvasview/MainActivity.java
+++ p1411canvasview/src/main/java/ru/startandroid/p1411canvasview/MainActivity.java
@@ -1,11 +1,14 @@
 package ru.startandroid.p1411canvasview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,15 +19,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(new DrawView(this));
     }
 
-    class DrawView extends View {
+    class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+
+        private DrawThread drawThread;
 
         public DrawView(Context context) {
             super(context);
+            getHolder().addCallback(this);
         }
 
         @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawColor(Color.GREEN);
+        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+            drawThread = new DrawThread(getHolder());
+            drawThread.setRunning(true);
+            drawThread.start();
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+            boolean retry = true;
+            drawThread.setRunning(false);
+            while (retry) {
+                try {
+                    drawThread.join();
+                    retry = false;
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+        class DrawThread extends Thread {
+            private boolean running = false;
+            private SurfaceHolder surfaceHolder;
+
+            public DrawThread(SurfaceHolder surfaceHolder) {
+                this.surfaceHolder = surfaceHolder;
+            }
+
+            public void setRunning(boolean running) {
+                this.running = running;
+            }
+
+            @Override
+            public void run() {
+                Canvas canvas;
+                while (running) {
+                    canvas = null;
+                    try {
+                        canvas = surfaceHolder.lockCanvas(null);
+                        if (canvas == null) continue;
+                        canvas.drawColor(Color.GREEN);
+                    } finally {
+                        if (canvas != null) {
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        }
+                    }
+                }
+            }
         }
     }
 }
